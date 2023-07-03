fe2d91fd2071752470f9f865e7eb08e1cafff6a1
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-07-03 15:36:51 +0900

    첫번째 과제 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const headArr = [];
+
+  const returnValues = (num) => {
+    let data = [];
+    for (let j = 1; j <= 9; j++) {
+      data.push(
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {num} * {j} = {num * j}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      );
+    }
+    return <div>{data}</div>;
+  };
+
+  const arrMult = () => {
+    for (let i = 2; i <= 9; i++) {
+      headArr.push(i);
+    }
+  };
+  arrMult();
+  return (
+    <div className="app">
+      {/* 1번 문제 - 계산기 */}
+      <div className="calculator">
+        <div className="value-area">
+          <p>11</p>
+        </div>
+
+        <div className="contents-area">
+          <ul className="button-area">
+            <li>
+              <button>c</button>
+            </li>
+            <li>
+              <button>+/-</button>
+            </li>
+            <li>
+              <button>%</button>
+            </li>
+            <li>
+              <button>/</button>
+            </li>
+          </ul>
+
+          <ul className="button-area">
+            <li>
+              <button>7</button>
+            </li>
+            <li>
+              <button>8</button>
+            </li>
+            <li>
+              <button>9</button>
+            </li>
+            <li>
+              <button>*</button>
+            </li>
+          </ul>
+
+          <ul className="button-area">
+            <li>
+              <button>4</button>
+            </li>
+            <li>
+              <button>5</button>
+            </li>
+            <li>
+              <button>6</button>
+            </li>
+            <li>
+              <button>-</button>
+            </li>
+          </ul>
+
+          <ul className="button-area">
+            <li>
+              <button>1</button>
+            </li>
+            <li>
+              <button>2</button>
+            </li>
+            <li>
+              <button>3</button>
+            </li>
+            <li>
+              <button>+</button>
+            </li>
+          </ul>
+
+          <ul className="button-area">
+            <li className="w2">
+              <button>0</button>
+            </li>
+            <li>
+              <button>.</button>
+            </li>
+            <li>
+              <button>=</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* 2번 문제 - 구구단 */}
+      <div className="multiplication">
+        {headArr.map((value, i) => (
+          <div key={value}>
+            <div>
+              <p>{value} 단</p>
+            </div>
+
+            {returnValues(i + 2)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
